c5ad82170d0ba9a53144e4bc1f9f7a4d5625eea7
attila832558 <ubikutya2003@gmail.com>
2022-11-21 15:34:39 +0100

    Initial commit

---
+++ src/test/java/com/example/szakdolgozatadatmodell/SzakdolgozatAdatmodellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szakdolgozatadatmodell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzakdolgozatAdatmodellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
